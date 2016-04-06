c1089100d8cee9294d2282499c2d3b08b299a472
Aris Xanthos <aris.xanthos@unil.ch>
2016-04-06 22:45:11 +0200

    title list is properly cached and selected label is saved

--- theatre_classique/MyOrangeProject/MyOrangeWidgets/OWTextableTheatreClassique.py
+++ theatre_classique/MyOrangeProject/MyOrangeWidgets/OWTextableTheatreClassique.py
@@ -5,7 +5,7 @@
 <priority>10</priority>
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.0001'
 
 import Orange
 from OWWidget import *
@@ -19,6 +19,9 @@ from _textable.widgets.TextableUtils import *   # Provides several utilities.
 
 import urllib2
 import re
+import inspect
+import os
+import pickle
 
 
 class OWTextableTheatreClassique(OWWidget):
@@ -31,6 +34,7 @@ class OWTextableTheatreClassique(OWWidget):
         'autoSend',
         'label',
         'uuid',
+        'selectedTitleLabels',
         'filterCriterion',
         'filterValue',
         'displayAdvancedSettings',
@@ -50,7 +54,9 @@ class OWTextableTheatreClassique(OWWidget):
         self.autoSend = True
         self.label = u'xml_tei_data'
         self.filterCriterion = u'none'
-        self.filterValue = None     
+        self.filterValue = None 
+        self.titleLabels = list()
+        self.selectedTitleLabels = list()
         self.displayAdvancedSettings = False
 
         # Always end Textable widget settings with the following 3 lines...
@@ -59,8 +65,6 @@ class OWTextableTheatreClassique(OWWidget):
         self.uuid = getWidgetUuid(self)
 
         # Other attributes...
-        self.titleLabels = list()
-        self.selectedTitleLabels = list()
         self.segmenter = Segmenter()
         self.segmentation = Input()
         self.titleSeg = None
@@ -103,7 +107,6 @@ class OWTextableTheatreClassique(OWWidget):
             widget=self.controlArea,
             box=u'Filter',
             orientation='vertical',
-            addSpace=True,
         )
         filterCriterionCombo = OWGUI.comboBox(
             widget=filterBox,
@@ -127,7 +130,7 @@ class OWTextableTheatreClassique(OWWidget):
             master=self,
             value='filterValue',
             orientation='horizontal',
-            label=u'Author:',
+            label=u'Value:',
             labelWidth=180,
             callback=self.sendButton.settingsChanged,
             tooltip=(
@@ -140,6 +143,7 @@ class OWTextableTheatreClassique(OWWidget):
         # The following lines add filterBox (and a vertical separator) to the
         # advanced interface...
         self.advancedSettings.advancedWidgets.append(filterBox)
+        self.advancedSettings.advancedWidgetsAppendSeparator()
 
         # Title box
         titleBox = OWGUI.widgetBox(
@@ -161,7 +165,7 @@ class OWTextableTheatreClassique(OWWidget):
             widget=titleBox,
             master=self,
             label=u'Refresh',
-            callback=self.getTitleListFromTheatreClassique,
+            callback=self.refreshTitleSeg,
             tooltip=u"Connect to Theatre-classique website and refresh list.",
         )
         OWGUI.separator(widget=titleBox, height=3)
@@ -176,9 +180,10 @@ class OWTextableTheatreClassique(OWWidget):
         # Now Info box and Send button must be drawn...
         self.infoBox.draw()
         self.sendButton.draw()
-
-        # Load Theatre-classique title list.
-        self.getTitleListFromTheatreClassique()
+        
+        # This initialization step needs to be done after infoBox has been 
+        # drawn (because getTitleSeg may need to display an error message).
+        self.getTitleSeg()
 
         # Send data if autoSend.
         self.sendButton.sendIf()
@@ -232,8 +237,7 @@ class OWTextableTheatreClassique(OWWidget):
 
         # Send token...
         self.send('Text data', self.segmentation, self)
-        self.sendButton.resetSettingsChangedFlag()
-        
+        self.sendButton.resetSettingsChangedFlag()        
         
     def updateGUI(self):
         """Update GUI state"""
@@ -241,14 +245,50 @@ class OWTextableTheatreClassique(OWWidget):
             self.advancedSettings.setVisible(True)
         else:
             self.advancedSettings.setVisible(False)
+            
+        if len(self.titleLabels) > 0:
+            self.selectedTitleLabels = self.selectedTitleLabels
+            
+    def getTitleSeg(self):
+        """Get title segmentation, either saved locally or online"""
+        
+        # Try to open saved file in this module's directory...
+        path = os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe()))
+        )
+        try:
+            file = open(os.path.join(path, "cached_title_list"),'r')
+            self.titleSeg = pickle.load(file)
+            file.close()
+        # Else try to load list from Theatre-classique and build new seg...
+        except IOError:
+            self.titleSeg = self.getTitleListFromTheatreClassique()
 
+        # Update title list and filter comboboxes (only at init and on manual
+        # refresh, therefore separate from self.updateGUI).
+        self.updateLists()
+                    
+    def refreshTitleSeg(self):
+        """Refresh title segmentation from website"""
+        self.titleSeg = self.getTitleListFromTheatreClassique()
+        # Update title list and filter comboboxes (only at init and on manual
+        # refresh, therefore separate from self.updateGUI).
+        self.updateLists()
+        
     def getTitleListFromTheatreClassique(self):
         """Fetch titles from the Theatre-classique website"""
 
+        self.infoBox.customMessage(
+            u'Fetching data from Theatre-classique website, please wait'
+        )
+        
         # Attempt to connect to Theatre-classique...
         try:
             response = urllib2.urlopen(self.base_url)
             base_html = unicode(response.read(), 'iso-8859-1')
+            self.infoBox.customMessage(
+                u'Done fetching data from Theatre-classique website.'
+            )
 
         # If unable to connect (somehow)...
         except:
@@ -263,8 +303,8 @@ class OWTextableTheatreClassique(OWWidget):
 
             # Reset output channel.
             self.send('Text data', None, self)
-            return
-
+            return None
+            
         # Otherwise store HTML content in LTTL Input object.
         base_html_seg = Input(base_html)
 
@@ -293,7 +333,7 @@ class OWTextableTheatreClassique(OWWidget):
         )
 
         # Parse each line and store the resulting segmentation in an attribute.
-        self.titleSeg = self.segmenter.tokenize(
+        titleSeg = self.segmenter.tokenize(
             segmentation=line_seg,
             regexes=[
                 (field_regex, 'Tokenize', {'author': '&1'}),
@@ -306,8 +346,32 @@ class OWTextableTheatreClassique(OWWidget):
         )
 
         # Sort this segmentation alphabetically based on titles...
-        self.titleSeg.segments.sort(key=lambda s: s.annotations['title'])
+        titleSeg.segments.sort(key=lambda s: s.annotations['title'])
         
+        # Try to save list in this module's directory for future reference...
+        path = os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe()))
+        )
+        try:
+            file = open(os.path.join(path, "cached_title_list"), 'wb')
+            pickle.dump(titleSeg, file) 
+            file.close()         
+        except IOError:
+            pass
+
+        # Remove warning (if any)...
+        self.error(0)
+        self.warning(0)
+        
+        return titleSeg
+
+    def updateLists(self):
+        """Update lists in the interface"""
+        
+        # If titleSeg has not been loaded for some reason, skip.
+        if self.titleSeg is None:
+            return
+            
         # Populate titleLabels list with the titles...
         self.titleLabels = sorted(
             [s.annotations['title'] for s in self.titleSeg]
@@ -322,9 +386,7 @@ class OWTextableTheatreClassique(OWWidget):
                 titleLabels[idx] = titleLabel + " (%s)" % author
         self.titleLabels = titleLabels
         
-        # Remove warning (if any)...
-        self.error(0)
-        self.warning(0)
+        self.updateGUI()
 
     def onDeleteWidget(self):
         """Make sure to delete the stored segmentation data when the widget
@@ -332,8 +394,8 @@ class OWTextableTheatreClassique(OWWidget):
         """
         self.segmentation.clear()
 
-        # The following two methods need to be copied (without any change) in
-        # every Textable widget...
+    # The following two methods need to be copied (without any change) in
+    # every Textable widget...
 
     def getSettings(self, *args, **kwargs):
         """Read settings, taking into account version number (overriden)"""
